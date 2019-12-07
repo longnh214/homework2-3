65666159c386e6e442956b2321096ef870670699
cnh6123 <cnh6123@naver.com>
2019-12-07 20:40:10 +0900

    add Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Fibonacci{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(fib(num));
+    }
+
+    public int fib(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+}
